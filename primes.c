f491e434566788bda9a55e9549f3bd1ee969314d
Dominic Perez <dominicp0309@gmail.com>
2020-06-22 15:50:03 +1000

    simplification

--- primes.c
+++ primes.c
@@ -1,4 +1,20 @@
-#include "primes.h"
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "bitarray.h"
+
+struct prime
+{
+        bitarray_t *nums;
+        long count;
+        long bound;
+};
+
+void destroy_prime(struct prime *p);
+struct prime *primes(long n);
+void write_to_file(struct prime *p);
 
 int main(int argc, char **argv)
 {
